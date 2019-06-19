e698050faaab71e7448593ef59334ba9910bbd7a
Shivam1097 <shivam.dev1097@gmail.com>
2019-06-19 10:57:40 +0530

    Initial Commit

---
+++ Bean Property/src/main/java/shivam/Tyre.java
@@ -0,0 +1,24 @@
+package shivam;
+
+/**
+ * created by Shivam on 19-06-2019.
+ */
+public class Tyre {
+
+    private String brand;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Tyre{" +
+                "brand='" + brand + '\'' +
+                '}';
+    }
+}
